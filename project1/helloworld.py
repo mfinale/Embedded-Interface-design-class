612a7c2836ccce513ca116610606ffc00b687f44
mfinale <finalemichael@gmail.com>
2019-09-15 13:21:00 -0400

    added main file. completing sql experimental code.

--- project1/helloworld.py
+++ project1/helloworld.py
@@ -1,7 +1,8 @@
 # installation steps for readme
 # clone from git
-# install mySQL drive for python pip3 install mysql-connector
+# install mySQL drive for python sudo pip3 install mysql-connector
 # install mySQL and create a local database with user and password info https://pimylifeup.com/raspberry-pi-mysql/
+# install DHT library for Raspberry Pi sudo pip3 install Adafruit_DHT
 import mysql.connector
 
 #create a database connection
@@ -10,8 +11,12 @@ mydb = mysql.connector.connect(host="localhost",user="eiduser",passwd="Shrek2",
 
 mycursor = mydb.cursor()
 
+#start with clean table called "sensordata"
+mycursor.execute("DROP TABLE sensordata")
+mycursor.execute("CREATE TABLE sensordata (temp float(4,2), humid float(4,2))")
 
-#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+
+
 
 mycursor.execute("SHOW TABLES")
 
@@ -19,15 +24,15 @@ for x in mycursor:
   print(x)
   
 
-sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-val = ("John", "Highway 21")
+sql = "INSERT INTO sensordata (temp, humid) VALUES (%s, %s)"
+val = (12.00, 17.55)
 mycursor.execute(sql, val)
 
 mydb.commit()
 
 print(mycursor.rowcount, "record inserted.")
 
-mycursor.execute("SELECT * FROM customers")
+mycursor.execute("SELECT * FROM sensordata")
 
 myresult = mycursor.fetchall()
 
