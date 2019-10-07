18251c3e6cc7d55b90cba585b63257242e849052
mfinale <finalemichael@gmail.com>
2019-10-07 09:56:07 -0400

    Update node_server.js
    Adding documentation.

--- project2/node_server.js
+++ project2/node_server.js
@@ -1,9 +1,29 @@
-// server.js
- 
+
+/*
+node_server.js: This file runs a node.js server on local host port 8080. The server establishes a websocket with 
+the client as well as a mySQL database connection with the database that is populated with sensor data from DHT22_application.py
+
+The server accepts 2 commands from the client: "Read one from db" which retrieves and sends the latest entry from the database, and
+"fetch 10 samples" which retrieves and sends the last 10 samples from the database.
+
+This file must be ran before opening the client. Please use launch.py to start this file.
+*/ 
 const WebSocket = require('ws')
 var mysql = require('mysql'); 
 
-//open webscoket on local host port 8080 
+/*
+__author__ = "Michael Finale"
+__copyright__ = "Copyright (C) 2019 by Michael Finale"
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are
+# permitted to modify this and use it to learn about the field of embedded
+# software. Michael Finale and the University of Colorado
+# are not liable for any misuse of this material.
+#
+*/
+
+//open webssocket on local host port 8080 
 const wss = new WebSocket.Server({ port: 8080 })
 //start connection to sensor db
 var db_con = mysql.createConnection({
@@ -29,14 +49,16 @@ wss.on('connection', ws => {
   ws.on('message', message => {
     console.log("Message from Client: "+ message);
     
-    //read one entry from my sql db
+    //Respond to "Read one from db" command from client.
     if (message=="Read one from db") {
       rows=1
-       console.log("Reading last " +rows + " entries from sensor db.");
+      console.log("Reading last " +rows + " entries from sensor db.");
+      //request all readings from the db.
       db_cmd="SELECT * FROM (SELECT * FROM sensordata ORDER BY timestamp DESC LIMIT " + rows + ") sub ORDER by timestamp ASC"
       db_con.query(db_cmd, function (err, result, fields) {
       if (err) throw err;
-      //only do the following if there is atleast one entry in mysql db. 
+      //Only attempt to parse the information from the most recent sb reading if there is at least on entry in the db.
+      //Otherwise an error is throne.
       if (result.length > 0){
         var time = result[0].timestamp;
         var temp= result[0].temp;
@@ -45,23 +67,28 @@ wss.on('connection', ws => {
         console.log(reading);
         ws.send('noderead'+reading);
         }
-      else {ws.send('noderead'+"Timestamp: no data in db      / Temperature : no data in db   / Humidity : no data in db  %");}
+       // Send this message if there is no data in the db.
+       else {ws.send('noderead'+"Timestamp: no data in db      / Temperature : no data in db   / Humidity : no data in db  %");}
       });
     } 
+   //Respond to "fetch 10 samples" command from client.
     else if (message=="fetch 10 samples") {
       rows=10
+      //retrieve the last "rows" from the mySQL db.
        console.log("Reading last " +rows + " entries from sensor db.");
       db_cmd="SELECT * FROM (SELECT * FROM sensordata ORDER BY timestamp DESC LIMIT " + rows + ") sub ORDER by timestamp ASC"
       db_con.query(db_cmd, function (err, result, fields) {
       if (err) throw err;
       multi_reading = ""
-      //increment only up to the current size of the sql sb. Otherwise an error is thrown if trying to get propert of empty db.
+      //increment only up to the current size of the sql sb. Otherwise an error is thrown if trying
+      // to get the property of an empty db entry.
       for (var i = 0; i <result.length; i++) {
       var time = result[i].timestamp;
       var temp= result[i].temp;
       var humid = result[i].humid;
      multi_reading = multi_reading + "(Timestamp: " + time + "      / Temperature : " + temp+"*C " + " / Humidity : "+ humid+"%)"
-    }
+     }
+      //send information back
       ws.send('nodefetch'+multi_reading);
       });
     } 
