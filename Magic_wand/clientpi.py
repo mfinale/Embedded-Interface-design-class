691d43f01bb9bebc9644c1ef30acd5dea22d1e98
mfinale <finalemichael@gmail.com>
2019-11-10 17:18:17 -0500

    testing polly function

--- Magic_wand/clientpi.py
+++ Magic_wand/clientpi.py
@@ -5,7 +5,7 @@ sqs = boto3.resource('sqs')
 polly = boto3.client('polly')
 for queue in sqs.queues.all():
     print(queue.url)
-response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3',  Text = 'This is a sample text to be synthesized.')
+response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3',  Text = 'put a bird on it.')
 
 file = open('speech.mp3', 'wb')
 file.write(response['AudioStream'].read())
