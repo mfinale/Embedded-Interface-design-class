5b70f76f3e39d619d61c035fe65b4693565127b7
mfinale <finalemichael@gmail.com>
2019-11-10 20:04:34 -0500

    adding speech to text function. still need to retrieved data from s3

--- Magic_wand/README.md
+++ Magic_wand/README.md
@@ -1,1 +1,4 @@
+setup boto and aws credentials:
 http://2017.compciv.org/guide/topics/aws/intro-to-aws-boto3.html#installing-the-aws-tools
+create an s3 bucket : magic-wand-bucket
+https://realpython.com/python-boto3-aws-s3/
--- Magic_wand/clientpi.py
+++ Magic_wand/clientpi.py
@@ -1,13 +1,42 @@
 import boto3
+import time
 
-# Let's use Amazon S3
-sqs = boto3.resource('sqs')
+
+
 polly = boto3.client('polly')
-for queue in sqs.queues.all():
-    print(queue.url)
-response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3',  Text = 'put a bird on it.')
+s3 = boto3.resource('s3')
+transcribe = boto3.client('transcribe')
 
-file = open('speech.mp3', 'wb')
-file.write(response['AudioStream'].read())
-file.close()
-print ("fuck you")
+
+
+def text_to_speech (text):
+    response = polly.synthesize_speech(VoiceId='Joanna', OutputFormat='mp3',  Text = text)
+    file = open('speech.mp3', 'wb')
+    file.write(response['AudioStream'].read())
+    file.close()
+
+def speech_to_text(audio_file_name):
+    s3.Object('magic-wand-bucket', audio_file_name).delete()
+    s3.Object('magic-wand-bucket', audio_file_name).upload_file(Filename=audio_file_name)
+    job_name = "my_job"
+    job_uri = "https://magic-wand-bucket.s3.amazonaws.com/"+audio_file_name
+    transcribe.delete_transcription_job(TranscriptionJobName=job_name)
+    transcribe.start_transcription_job(
+        TranscriptionJobName=job_name,
+        Media={'MediaFileUri': job_uri},
+        MediaFormat='mp3',
+        LanguageCode='en-US'
+    )
+    while True:
+        status = transcribe.get_transcription_job(TranscriptionJobName=job_name)
+        if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
+            break
+        print("Not ready yet...")
+        time.sleep(5)
+    print(status)
+
+
+
+text_to_speech('Hello there. What is up?')
+speech_to_text('speech.mp3')
+print ("done")
